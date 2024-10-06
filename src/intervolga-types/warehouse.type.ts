70aefc84996634d5fe3e9f940d033f7d36e2b2e7
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 02:33:43 +0300

    добавилgeo_lat: string,     geo_lon: string для доступа к координатам, рассчитанным на сервере

--- src/intervolga-types/warehouse.type.ts
+++ src/intervolga-types/warehouse.type.ts
@@ -3,5 +3,7 @@ import { Dealer } from "./dealer.type.ts"
 export type Warehouse = {
     id: number,
     address: string,
-    dealer: Dealer
+    dealer: Dealer,
+    geo_lat: string,
+    geo_lon: string
 }
