29ec88bc434ede92b94750c67e2d99a30afd4a61
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 02:35:22 +0300

    добавил типизацию объекта настроек валидации предварительной формы

---
+++ src/intervolga-types/validity.type.ts
@@ -0,0 +1,14 @@
+/**
+   * @description Типизирует объект настроек валидации предварительной формы
+   * @date 07/10/2024/02:24:36
+   * @author Kuyantsev Aleksandr https://dixie-34.ru https://vk.com/karkade2021 https://t.me/d_e_p_L_o_y_1
+*/
+export type Validity = {
+    firstName: boolean;
+    address: boolean;
+    goods: boolean;
+    cost: boolean;
+    quantity: boolean;
+    total: boolean;
+    suppliers: boolean;
+}
