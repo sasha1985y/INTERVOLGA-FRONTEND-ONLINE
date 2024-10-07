cb3793dcce48f9334ddd83da7e7da163319238a2
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 20:34:34 +0300

    удаление ненужного закомментированного кода

--- src/handlers/handlechange.handler.ts
+++ src/handlers/handlechange.handler.ts
@@ -1,5 +1,7 @@
 import { Product } from "../intervolga-types";
 
+//import { calculateTotal } from "../api";
+
 /**
  * @description Обработчик определяет поведение полей в ответ на пользовательский ввод. Содержит настройки валидации к каждому полю.
  * @author Kuyantsev Aleksandr https://dixie-34.ru https://vk.com/karkade2021 https://t.me/d_e_p_L_o_y_1
@@ -39,6 +41,7 @@ import { Product } from "../intervolga-types";
                     cost: string;
                     quantity: string;
                     total: string;
+                    suppliers: string;
                 },
                 setFormData: React.Dispatch<React.SetStateAction<{
                     firstName: string;
@@ -47,6 +50,7 @@ import { Product } from "../intervolga-types";
                     cost: string;
                     quantity: string;
                     total: string;
+                    suppliers: string;
                 }>>,
                 setValidity: React.Dispatch<React.SetStateAction<{
                     firstName: boolean;
@@ -55,179 +59,193 @@ import { Product } from "../intervolga-types";
                     cost: boolean;
                     quantity: boolean;
                     total: boolean;
+                    suppliers: boolean;
                 }>>
             ) => {
                 const { name, value } = e.target;
                 const isValidCost = String(products.find(product => product.id === Number(value))?.price || '');
                 setFormData((prev) => ({
-                ...prev,
-                [name]: value,
-                ...(name === 'goods' && {
-                    cost: isValidCost,
-                }),
-                ...(name === 'quantity' && {
-                    total: String((products.find(product => product.id === Number(formData.goods))?.price || 0) * Number(value)),
-                }),
+                    ...prev,
+                    [name]: value,
+                    ...(name === 'goods' && {
+                        cost: isValidCost,
+                    }),
+                    ...(name === 'quantity' && {
+                        total: String((products.find(product => product.id === Number(formData.goods))?.price || 0) * Number(value)),
+                    }),
                 }));
-                
+
                 if (name === 'goods') {
-                const selectedProduct = products.find(product => product.id === Number(value));
-                const isValidCost = selectedProduct && selectedProduct.price > 0;
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    goods: value !== '' ? true : false,
-                    cost: isValidCost ? true : false,
-                    total: selectedProduct && prevValidity.quantity ? true : false
-                }));
+                    const selectedProduct = products.find(product => product.id === Number(value));
+                    const isValidCost = selectedProduct && selectedProduct.price > 0;
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        goods: value !== '' ? true : false,
+                        cost: isValidCost ? true : false,
+                        total: selectedProduct && prevValidity.quantity ? true : false
+                    }));
                 }
 
                 if (name === 'cost') {
-                const costPattern = /^\s*\d+(\.\d{2})?$/; // Проверяем формат 500.00
-                const isValidCost = value !== '' && costPattern.test(value)
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    cost: isValidCost,
-                }));
+                    const costPattern = /^\s*\d+(\.\d{2})?$/; // Проверяем формат 500.00
+                    const isValidCost = value !== '' && costPattern.test(value)
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        cost: isValidCost,
+                    }));
                 }
 
                 if (name === 'quantity') {
-                const quantityPattern = /^\s*[1-9]\d*$/;
-                const isValidQuantity = quantityPattern.test(value);
-                const isNotNullQuantity = value !== '0';
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    quantity: isValidQuantity,
-                    total: isNotNullQuantity && prevValidity.goods ? true : false
-                }));
+                    const quantityPattern = /^\s*[1-9]\d*$/;
+                    const isValidQuantity = quantityPattern.test(value);
+                    const isNotNullQuantity = value !== '0';
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        quantity: isValidQuantity,
+                        total: isNotNullQuantity && prevValidity.goods ? true : false
+                    }));
                 }
-            
+
                 if (name === 'total') {
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    total: value !== '' ? true : false// Проверяем, что поле total не пустое
-                }));
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        total: value !== '' ? true : false// Проверяем, что поле total не пустое
+                    }));
                 }
 
                 if (name === 'address') {
-                const addressPattern = /^[A-Za-zА-Яа-яЁё\s]+,\s*[A-Za-zА-Яа-яЁё\s]+\s*\d+$/;
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    address: addressPattern.test(value)
-                }));
+                    const addressPattern = /^[A-Za-zА-Яа-яЁё\s]+,\s*[A-Za-zА-Яа-яЁё\s]+\s*\d+$/;
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        address: addressPattern.test(value)
+                    }));
                 }
 
                 if (name === 'firstName') {
-                const firstNamePattern = /^[A-Za-zА-Яа-яЁё\s]+$/;
-                setValidity((prevValidity) => ({
-                    ...prevValidity,
-                    firstName: firstNamePattern.test(value)
-                }));
+                    const firstNamePattern = /^[A-Za-zА-Яа-яЁё\s]+$/;
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        firstName: firstNamePattern.test(value)
+                    }));
+                }
+
+                if (name === 'suppliers') {
+                    setValidity((prevValidity) => ({
+                        ...prevValidity,
+                        suppliers: value !== '' ? true : false
+                    }));
                 }
             };
  */
 export const handleChange = (
-  e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
-  products: Product[],
-  formData: {
-    firstName: string;
-    address: string;
-    goods: string;
-    cost: string;
-    quantity: string;
-    total: string;
-    suppliers: string;
-  },
-  setFormData: React.Dispatch<React.SetStateAction<{
-    firstName: string;
-    address: string;
-    goods: string;
-    cost: string;
-    quantity: string;
-    total: string;
-    suppliers: string;
-  }>>,
-  setValidity: React.Dispatch<React.SetStateAction<{
-    firstName: boolean;
-    address: boolean;
-    goods: boolean;
-    cost: boolean;
-    quantity: boolean;
-    total: boolean;
-    suppliers: boolean;
-  }>>
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    products: Product[],
+    formData: {
+        firstName: string;
+        address: string;
+        goods: string;
+        cost: string;
+        quantity: string;
+        total: string;
+        suppliers: string;
+    },
+    setFormData: React.Dispatch<React.SetStateAction<{
+        firstName: string;
+        address: string;
+        goods: string;
+        cost: string;
+        quantity: string;
+        total: string;
+        suppliers: string;
+    }>>,
+    setValidity: React.Dispatch<React.SetStateAction<{
+        firstName: boolean;
+        address: boolean;
+        goods: boolean;
+        cost: boolean;
+        quantity: boolean;
+        total: boolean;
+        suppliers: boolean;
+    }>>,
+    //orders: Order[],
+    //setError: React.Dispatch<React.SetStateAction<string | null>>
 ) => {
-  const { name, value } = e.target;
-  const isValidCost = String(products.find(product => product.id === Number(value))?.price || '');
-  setFormData((prev) => ({
-    ...prev,
-    [name]: value,
-    ...(name === 'goods' && {
-      cost: isValidCost,
-    }),
-    ...(name === 'quantity' && {
-      total: String((products.find(product => product.id === Number(formData.goods))?.price || 0) * Number(value)),
-    }),
-  }));
-
-  if (name === 'goods') {
-    const selectedProduct = products.find(product => product.id === Number(value));
-    const isValidCost = selectedProduct && selectedProduct.price > 0;
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      goods: value !== '' ? true : false,
-      cost: isValidCost ? true : false,
-      total: selectedProduct && prevValidity.quantity ? true : false
+    const { name, value } = e.target;
+    const isValidCost = String(products.find(product => product.id === Number(value))?.price || '');
+    setFormData((prev) => ({
+        ...prev,
+        [name]: value,
+        ...(name === 'goods' && {
+            cost: isValidCost,
+        }),
+        ...(name === 'quantity' && {
+            total: String((products.find(product => product.id === Number(formData.goods))?.price || 0) * Number(value)),
+        }),
     }));
-  }
 
-  if (name === 'cost') {
-    const costPattern = /^\s*\d+(\.\d{2})?$/; // Проверяем формат 500.00
-    const isValidCost = value !== '' && costPattern.test(value)
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      cost: isValidCost,
-    }));
-  }
+    if (name === 'goods') {
+        const selectedProduct = products.find(product => product.id === Number(value));
+        const isValidCost = selectedProduct && selectedProduct.price > 0;
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            goods: value !== '' ? true : false,
+            cost: isValidCost ? true : false,
+            total: selectedProduct && prevValidity.quantity ? true : false
+        }));
+    }
 
-  if (name === 'quantity') {
-    const quantityPattern = /^\s*[1-9]\d*$/;
-    const isValidQuantity = quantityPattern.test(value);
-    const isNotNullQuantity = value !== '0';
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      quantity: isValidQuantity,
-      total: isNotNullQuantity && prevValidity.goods ? true : false
-    }));
-  }
+    if (name === 'cost') {
+        const costPattern = /^\s*\d+(\.\d{2})?$/; // Проверяем формат 500.00
+        const isValidCost = value !== '' && costPattern.test(value)
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            cost: isValidCost,
+        }));
+    }
 
-  if (name === 'total') {
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      total: value !== '' ? true : false// Проверяем, что поле total не пустое
-    }));
-  }
+    if (name === 'quantity') {
+        const quantityPattern = /^\s*[1-9]\d*$/;
+        const isValidQuantity = quantityPattern.test(value);
+        const isNotNullQuantity = value !== '0';
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            quantity: isValidQuantity,
+            total: isNotNullQuantity && prevValidity.goods ? true : false
+        }));
+    }
+    
+    if (name === 'total' /*&& ["quantity", "cost", "address", "suppliers"].includes(name)*/) {
+        //calculateTotal(formData, setFormData, orders);
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            total: value !== '' ? true : false// Проверяем, что поле total не пустое
+        }));
+    }
 
-  if (name === 'address') {
-    const addressPattern = /^[A-Za-zА-Яа-яЁё\s]+,\s*[A-Za-zА-Яа-яЁё\s]+\s*\d+$/;
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      address: addressPattern.test(value)
-    }));
-  }
+    if (name === 'address') {
+        const addressPattern = /^[A-Za-zА-Яа-яЁё\s]+,\s*[A-Za-zА-Яа-яЁё\s]+\s*\d+$/;
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            address: addressPattern.test(value)
+        }));
+    }
 
-  if (name === 'firstName') {
-    const firstNamePattern = /^[A-Za-zА-Яа-яЁё\s]+$/;
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      firstName: firstNamePattern.test(value)
-    }));
-  }
+    if (name === 'firstName') {
+        const firstNamePattern = /^[A-Za-zА-Яа-яЁё\s]+$/;
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            firstName: firstNamePattern.test(value)
+        }));
+    }
 
-  if (name === 'suppliers') {
-    setValidity((prevValidity) => ({
-      ...prevValidity,
-      suppliers: value !== '' ? true : false
-    }));
-  }
+    if (name === 'suppliers') {
+        setValidity((prevValidity) => ({
+            ...prevValidity,
+            suppliers: value !== '' ? true : false
+        }));
+    }
+
+    // if (["quantity", "cost", "address", "suppliers"].includes(name)) {
+    //     calculateTotal(formData, setFormData, setError, orders);
+    // }
 };
-  
