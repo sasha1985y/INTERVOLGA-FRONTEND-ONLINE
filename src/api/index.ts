eba6068d19b32117c610873243ae954ebe849942
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 21:08:50 +0300

    реэкспорт calculateDelivery

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,3 +2,4 @@ export { fetchSafeServerOrders } from './safe-server-orders';
 export { fetchFromPrimarySource } from './fetch-primary';
 export { fetchFromLocalSource } from './fetch-local-orders';
 export { Timer } from './timer';
+export { calculateDelivery } from './calculate-delivery';
