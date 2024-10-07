816a93af4ba9a2a7abe9b5fa50806ddd56e2a7ed
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 20:32:27 +0300

    реэкспорт handleRadioChange

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -3,3 +3,4 @@ export { goOpenViewOrdersHandler } from './goorders.handler';
 export { goOpenFormHandler } from './goform.handler';
 export { handleKeyDown } from './keydown.handler';
 export { handleChange } from './handlechange.handler';
+export { handleRadioChange } from './radiochange.handler';
