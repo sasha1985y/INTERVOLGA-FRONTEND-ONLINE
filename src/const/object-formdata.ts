2ae000c8295d1be714ea31656525ab142673816f
sasha1985y <kuyancev1985@mail.ru>
2024-10-07 20:49:14 +0300

    добавил документацию Объекта настроек начального состояния валидации полей предварительной формы

---
+++ src/const/object-formdata.ts
@@ -0,0 +1,26 @@
+//Типы
+import { FormData } from "../intervolga-types"
+
+/**
+   * @description Объект настроек начального состояния полей предварительной формы
+   * @date 07/10/2024/00:54:11
+   * @author Kuyantsev Aleksandr https://dixie-34.ru https://vk.com/karkade2021 https://t.me/d_e_p_L_o_y_1
+   * @example const FORMDATA: FormData = {//Начальное состояние полей предварительной формы
+                firstName: '',
+                address: '',
+                goods: '',
+                cost: '',
+                quantity: '0',
+                total: '',
+                suppliers: ''
+            }
+*/
+export const FORMDATA: FormData = {
+    firstName: '',
+    address: '',
+    goods: '',
+    cost: '',
+    quantity: '0',
+    total: '',
+    suppliers: ''
+}
